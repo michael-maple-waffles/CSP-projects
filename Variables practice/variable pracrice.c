4038526bd55dcc7d3f55851f5edb14d126384a9d
michael-maple-waffles <michael.willes@ucas-edu.net>
2024-09-09 20:28:58 +0000

    finished the variables work and saving file

--- Variables practice/variable pracrice.c
+++ Variables practice/variable pracrice.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 
 int main(viod){
-    char name[] = "michael"
-    printf("s%",name)
-    char 
+    char name[] = "michael";
+    printf("s%",name);
+    char numsml[] = "5";
+    printf("s%",numsml);
+    char numlrg[] = "125";
+    printf("s%",numlrg);
+    char breakfast[] = "oatmeal";
+    printf("s%",breakfast);
+    char favColor[] = "blue";
+    printf("s%",favColor);
+    char school[] = "UCAS";
+    printf("s%",school);
+    char year[] = "2024";
+    printf("s%",year);
+    char eyeColor[] = "blue";
+    printf("s%",eyeColor);
+    char age[] = "14";
+    printf("s%",age);
+    char subject[] = "math";
+    printf("s%",subject);
     return 0;
 }
