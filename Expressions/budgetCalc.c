c902f029caba638dfc6bb8751ced9cb0f7ea2b21
michael-maple-waffles <michael.willes@ucas-edu.net>
2024-09-12 20:42:35 +0000

    making the budget calculator

---
+++ Expressions/budgetCalc.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    float income,rent,utilities,groceries,transportation, total_expenses, savings, total;
+    float prent,putilities,ptransportation, pgroceries,pexpenses;
+    printf("this is going to calculate your budget for the month\n");
+    printf("how much do you make a month?:");
+    scanf("%f", &income);
+    printf("how much is rent?:");
+    scanf("%f", &rent);
+    printf("how much do you spend on utilities?:");
+    scanf("%f", &utilities);
+    printf("how much do you spend on groceries?:");
+    scanf("%f", &groceries);
+    printf("how much do you spend on transportation?:");
+    scanf("%f", &transportation);
+    total_expenses = rent + utilities +groceries + transportation;
+    savings = income *.2;
+    total = income - total_expenses - savings;
+    prent = rent / income;
+    putilities = utilities / income;
+    pgroceries = groceries / income;
+    ptransportation = transportation / income;
+    pexpenses = total_expenses / income;
+    printf("your income is:$%.2f\n", income);
+    printf("your expenses are:$%.2f\n", total_expenses);
+    printf("your savings are:$%.2f\n", savings);
+    printf("your total leaft to spend is:$%.2f\n", total);
+    printf("your rent is %.2f",prent, "of your income");
+    printf("your utilities is %.2f",putilities, "of your income");
+    printf("your groceries are %.2f",pgroceries, "of your income");
+    printf("your transportation is %.2f",ptransportation, "of your income");
+    printf("your expenses are %.2f",pexpenses, "of your income");
+    return 0;
+}
