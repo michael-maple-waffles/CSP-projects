fbcdfe1a256a15aeb7059aa462f9baf6c06c7e5c
michael-maple-waffles <michael.willes@ucas-edu.net>
2024-09-20 20:15:57 +0000

    Bullying Mara

--- Strings/nameDecorator.c
+++ Strings/nameDecorator.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
 int main(void) {
-    char sentence,name;
+    char sentence[200],name[20];
+    printf("what is your name?: ");
+    scanf("%s", name);
+    strcat(sentence,"((((");
+    strcat(sentence, name);
+    strcat(sentence,"))))\n");
+    strcat(sentence, "<<<<");
+    strcat(sentence, name);
+    strcat(sentence,">>>>\n");
+    strcat(sentence, "----");
+    strcat(sentence, name);
+    strcat(sentence, "----\n");
+    strcat(sentence, ":):)");
+    strcat(sentence, name);
+    strcat(sentence, "(:(:\n");
+    strcat(sentence, "####");
+    strcat(sentence, name);
+    strcat(sentence, "####\n");
+    strcat(sentence, "++++");
+    strcat(sentence, name);
+    strcat(sentence, "++++\n");
+    strcat(sentence, "====");
+    strcat(sentence, name);
+    strcat(sentence, "====\n");
+    strcat(sentence, "~~~~");
+    strcat(sentence, name);
+    strcat(sentence, "~~~~");
+    printf("%s",sentence);
     return 0;
 }
