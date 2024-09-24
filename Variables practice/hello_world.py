fdaea31b7341b014633a4cdfdaae6e93f34bda6e
michael-maple-waffles <michael.willes@ucas-edu.net>
2024-09-24 20:06:28 +0000

    changing hello_world.py

--- Variables practice/hello_world.py
+++ Variables practice/hello_world.py
@@ -1,3 +1,10 @@
-name = input("what is your name?: ")
-
-print("Hello "+name+"!")
+def nameAsk():
+    print("Hello "+ input("what is your name?: "))
+def runName():
+    nameAsk()
+    check = input("would you like to input a different name\n if so press y: ").strip().lower()
+    if check == "y":
+        nameAsk()
+    else:
+        runName
+runName()
