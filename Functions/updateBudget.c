99c50cbb84f23b1b5a0a6598835e639d9142e831
michael-maple-waffles <michael.willes@ucas-edu.net>
2024-09-30 20:13:55 +0000

    :(

--- Functions/updateBudget.c
+++ Functions/updateBudget.c
@@ -36,15 +36,5 @@ int main(void) {
     printf("your expenses are:$%.2f\n", total_expenses);
     printf("your savings are:$%.2f\n", savings);
     printf("your total leaft to spend is:$%.2f\n", total);
-    printf("your rent is  %.2f", rent);
-    printf("%% of your total income\n");
-    printf("your utilities is %.2f",utilities );
-    printf("%% of your total income\n");
-    printf("your groceries are %.2f",groceries);
-    printf("%% of your total income\n");
-    printf("your transportation is %.2f",transportation);
-    printf("%% of your total income\n");
-    printf("your expenses are %.2f",total_expenses);
-    printf("%% of your total income");
     return 0;
 }
