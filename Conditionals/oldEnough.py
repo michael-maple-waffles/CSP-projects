5c884c8dbaf9fe13485b9cc83a1ec0ba795421c2
michael-maple-waffles <michael.willes@ucas-edu.net>
2024-10-01 20:17:14 +0000

    >:(

--- Conditionals/oldEnough.py
+++ Conditionals/oldEnough.py
@@ -10,3 +10,16 @@ if age >= 5:
         print("Go get your learners Permit!")
 else:
     print("go away child!")
+
+#alternative for Mrs.Larose!
+
+if age >= 18:
+    print("\nGuess what! You can Vote!")
+elif age >= 16:
+    print("\nBelieve it or not! You can drive")
+elif age >= 15:
+    print("\nA drievrs license for You!")
+elif age >= 5:
+    print("\nGo back to school... Because You can!")
+else:
+    print("\nGo away child!")
