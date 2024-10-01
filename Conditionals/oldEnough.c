8c14f26e90058b38c91fcef15745900ff9f0348e
michael-maple-waffles <michael.willes@ucas-edu.net>
2024-10-01 20:41:23 +0000

    :)

---
+++ Conditionals/oldEnough.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    int age;
+    printf("what is your name?:\n");
+    scanf("%d",age);
+    if (age >=18){
+        printf("you are old enough to vote!");
+    }else if (age >= 16){
+        printf("your can drive!");
+    }else if (age >= 15){
+        
+    }
+
+    return 0;
+}
