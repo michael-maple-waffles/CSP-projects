aa2aac148f032758bb71aa647e907d73fc8b3a82
michael-maple-waffles <michael.willes@ucas-edu.net>
2024-10-02 20:41:15 +0000

    T^T

--- Conditionals/oldEnough.c
+++ Conditionals/oldEnough.c
@@ -2,14 +2,18 @@
 
 int main(void) {
     int age;
-    printf("what is your name?:\n");
-    scanf("%d",age);
+    printf("what is your age?:\n");
+    scanf("%d",&age);
     if (age >=18){
         printf("you are old enough to vote!");
     }else if (age >= 16){
-        printf("your can drive!");
+        printf("you can drive!");
     }else if (age >= 15){
-        
+        printf("Go get your learners permit!");
+    }else if (age >= 5){
+        printf("At least you can go to school!");
+    }else{
+        printf("Go away child!");
     }
 
     return 0;
