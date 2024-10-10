39d6374603a00c738461755379bf809a6f08d0e2
michael-maple-waffles <michael.willes@ucas-edu.net>
2024-10-10 20:42:11 +0000

    Blahhhh

--- Loops/fizzBuzz.c
+++ Loops/fizzBuzz.c
@@ -1,21 +1,41 @@
 #include <stdio.h>
-int i;
+#include <string.h>
+#include <time.h>
+int i, usr;
+char word1[50], word2[50], word3[50];
+
+void delay(int sec){
+    int milli = 1000 * sec;
+    clock_t start = clock();
+    while(clock()<start+milli);
+}
+
+
 int main(void) {
+    printf("how long do you want this?: \n");
+    scanf("%d", &usr);
+    printf("give me a word: \n");
+    scanf("%s", word1);
+    printf("give me another word: \n");
+    scanf("%s", word2);
+    strcat(word3, word1);
+    strcat(word3,word2);
     //make a loop that counts to 50
-    for (i=1; i<=50; i++){
+    for (i=1; i<=usr; i++){
         //replace numbers divisableby 3 and 5 with fizz buzz
         if ( i % 3 == 0 && i % 5 == 0){
-            printf("FizzBuzz\n");
+            printf("%s\n", word3);
             //replace divisable by threes
         }else if (i % 3 == 0){
-            printf("Fizz\n");
+            printf("%s\n",word1);
             //replace divisable by fives
         }else if (i % 5 == 0){
-            printf("Buzz\n");
+            printf("%s\n",word2);
             //replace every other number
         }else{
             printf("%d\n", i);
         }
+        delay(10);
     }
     return 0;
 }
