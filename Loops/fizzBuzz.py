ee87c920ccfa09aab4f39baffe112bba3fa65b28
michael-maple-waffles <michael.willes@ucas-edu.net>
2024-10-11 19:58:40 +0000

    why words >:(

--- Loops/fizzBuzz.py
+++ Loops/fizzBuzz.py
@@ -1,3 +1,14 @@
 i = 1
 while i <= 50:
-    
+    if i%3 == 0 and i%5 == 0:
+        print("FizzBuzz")
+        i += 1
+    elif i % 3 == 0:
+        print("Fizz")
+        i += 1
+    elif i % 5 == 0:
+        print("Buzz")
+        i += 1
+    else:
+        print(i)
+        i +=1
